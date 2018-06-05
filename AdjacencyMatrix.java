016e0dabc4957b84285825c2d9a14e30ba3004dd
Arpan Bhowmik <arpan.bhowmick9@gmail.com>
2018-06-05 23:09:50 +0530

    Created Class AdjacencyMatrix

---
+++ AdjacencyMatrix.java
@@ -0,0 +1,56 @@
+package JGraph;
+
+// To Create An Adjacency Matrix For A Graph
+// If The Maximum Number Of Vertices Be N. Then The Graph Will Have Vertex Values From 0 To N-1
+
+// Importing required libraries
+import java.io.*;
+import java.util.*;
+
+public class AdjacencyMatrix{
+    
+    public int N; // To store the total number of vertices
+    public int[][] adj_matrix; // Adjacency Matrix
+    public AdjacencyMatrix(int n){ // Constructor  n : Total Number Of Vertices
+        N=n;
+        adj_matrix=new int[N][N];
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++)
+                adj_matrix[i][j]=0;
+        }
+    }
+    
+    public int[][] create_edge(int from, int to, boolean weighted, int weight){ // To create the edge
+        // from : Source Vertex,  to : Destination Vertex,  weighted : Whether it is a weighted edge,  weight : If weighted is true then value of weight
+        try{
+            if(weighted==true) // If weighted edge
+                adj_matrix[from][to]=weight;
+            else
+                adj_matrix[from][to]=1;
+        }
+        catch(ArrayIndexOutOfBoundsException index){ // If invalid index
+            System.out.println(" Invalid Vertex!");
+        }
+        return adj_matrix;
+    }
+    
+    public int get_edge(int from, int to){ // To get the value of the edge
+        // from : Source Vertex,  to : Destination Vertex
+        int temp=0;
+        try{
+            temp=adj_matrix[from][to];
+        }
+        catch(ArrayIndexOutOfBoundsException index){ // If invalid index
+            System.out.println(" Invalid Vertex!");
+        }
+        return temp;
+    }
+    
+    public void display(){ // To display the adjacency matrix
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++)
+                System.out.print(adj_matrix[i][j]+" ");
+            System.out.println();
+        }
+    }
+}

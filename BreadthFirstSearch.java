d1f0fb5fc54fbb6d6f324b7738b74c87beb95977
Arpan Bhowmik <arpan.bhowmick9@gmail.com>
2018-06-06 17:38:56 +0530

    Added BreadthFirstSearch Class

---
+++ BreadthFirstSearch.java
@@ -0,0 +1,48 @@
+package JGraph;
+
+// Class To Compute The BreadthFirstSearch Traversal Of A Graph
+// If The Maximum Number Of Vertices Be N. Then The Graph Will Have Vertex Values From 0 To N-1
+
+// Importing required packages
+import java.io.*;
+import java.util.*;
+
+public class BreadthFirstSearch{
+    
+    public int[][] adj_matrix; // Adjacency Matrix
+    public int N; // Total number of vertices
+    Queue<Integer> queue;
+    Queue<Integer> bfs; // Will store the BreadthFirstSearch traversal
+    
+    public BreadthFirstSearch(int n, int[][] a){ // Constructor  n : Total number of vertices  a : Adjacency Matrix
+        N=n;
+        adj_matrix=new int[N][N];
+        adj_matrix=a;
+        queue=new LinkedList<Integer>();
+        bfs=new LinkedList<Integer>();
+    }
+    
+    public Queue<Integer> BFS(int s){ // To compute the BreadthFirstSearch traversal of the graph  s : Source vertex
+        boolean[] visited=new boolean[N];
+        int i=0,element=0;
+        
+        visited[s]=true; // Marking the vertex as visited
+        queue.add(s);
+        
+        while(!queue.isEmpty()){
+            element=queue.remove();
+            i=element;
+            bfs.add(element);
+            
+            while(i<N){
+                if((adj_matrix[element][i]>0)&&(visited[i]==false)){
+                    queue.add(i);
+                    visited[i]=true;
+                }
+                i++;
+            }
+        }
+        return bfs; // Returning the queue
+    }
+}
+        
